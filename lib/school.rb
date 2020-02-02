c2d8e94f4df4d7e870a0263438715f5d17a338cd
Charles Pelton <charliejp0311@gmail.com>
2020-02-02 19:07:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require "pry"
+class School
+  attr_accessor :school, :roster
+  
+  def initialize(name)
+    @school = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.include?(grade)
+      @roster[grade] << student
+    else 
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+  
+  def grade(num)
+    @roster[num]
+  end
+  
+  def sort
+    i = 0 
+    @roster.each do |stdnt|
+      binding.pry
+    end
+  end
+end 
